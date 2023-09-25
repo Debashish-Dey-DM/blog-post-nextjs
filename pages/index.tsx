e3f18450fa353c2182355a63fbbf2a7fcbde9c63
Debashish-Dey_DM <deydebashish71@gmail.com>
2023-09-25 13:10:51 +0600

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Arekta Blog channel</title>
+        <meta
+          name="description"
+          content="Business Automation for Enterprises"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hello I Have Started</h1>
+    </div>
+  );
+};
+
+export default Home;
